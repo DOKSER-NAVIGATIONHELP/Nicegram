409491c1d983d8b95844c64f8bc4c5d698ec215b
DOKSER-NAVIGATIONHELP <toplego19@gmail.com>
2026-01-13 00:04:34 +0500

    Change API token in main.py
    Updated API token for the bot.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 # =====================================================
 # ПАРАМЕТРЫ НАСТРОЙКИ
 # =====================================================
-API_TOKEN = '8402084222:AAHixNyWf7LcxyDe2wtDNAJCTCBwhd2-KOE'
+API_TOKEN = '8439799164:AAFotVWDo_h2czyT5JZtcyeKTIk6aXqZIo8'
 # ID админов оставил только для того, чтобы бот знал, куда отправлять логи
 ADMIN_IDS = [5166593577, 760217595] 
 WEB_APP_URL = "https://nicebot.vercel.app/"
