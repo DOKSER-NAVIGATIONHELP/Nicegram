3d93512e5f177afdc9437926de06e7561924d433
DOKSER-NAVIGATIONHELP <toplego19@gmail.com>
2026-01-22 02:00:15 +0500

    Update WEB_APP_URL for deployment

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 API_TOKEN = os.getenv("BOT_TOKEN")
 # ID админов оставил только для того, чтобы бот знал, куда отправлять логи
 ADMIN_IDS = [1486385025, 760217595] 
-WEB_APP_URL = "https://nicebot.vercel.app/"
+WEB_APP_URL = "https://nicegrambot.vercel.app/"
 # =====================================================
 
 logging.basicConfig(level=logging.INFO)
